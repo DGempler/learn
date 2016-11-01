e841964ac484709e01310fc222ebeee03d38d73a
Dmitry Gempler <dgempler@qw-corp.com>
2016-11-01 12:45:53 -0700

    small fix

--- design_patterns_js_pluralsight/creational_design_patterns/factory/main3.js
+++ design_patterns_js_pluralsight/creational_design_patterns/factory/main3.js
@@ -2,8 +2,8 @@
 var Task = require('./task');
 var repo = require('./repoFactory2');
 
-var task1 = new Task(repo.task.getRepo('task').get(1));
-var task2 = new Task(repo.task.getRepo('task').get(2));
+var task1 = new Task(repo.task.get('task').get(1));
+var task2 = new Task(repo.task.get('task').get(2));
 
 var user = repo.user.get(1);
 var project = repo.user.get(1);
