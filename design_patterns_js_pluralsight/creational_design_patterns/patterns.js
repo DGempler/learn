607ba58b019912bbb3eb54f9e7176a26c35894b0
Dmitry Gempler <dgempler@qw-corp.com>
2016-10-31 20:02:38 -0700

    added module pattern

--- design_patterns_js_pluralsight/creational_design_patterns/patterns.js
+++ design_patterns_js_pluralsight/creational_design_patterns/patterns.js
@@ -1,3 +1,17 @@
 /////////////////
 // Constructor Pattern
 /////////////////
+
+/////////////////
+// Module Pattern
+/////////////////
+
+var Module = function() {
+    var privateVar = 'I am private...';
+    return {
+        method: function()  {},
+        otherMethod: function() {
+
+        }
+    };
+};
